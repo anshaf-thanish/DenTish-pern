1a1d14c3d7ab650fd54686ff591d10a5030662d5
anshaf-thanish <anshafthanish@gmail.com>
2026-08-08 17:14:45 +0530

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import CTA from '@/components/landing/CTA'
+import Footer from '@/components/landing/Footer'
 import Header from '@/components/landing/Header'
 import Hero from '@/components/landing/Hero'
 import HowItWorks from '@/components/landing/HowItWorks'
 import PricingSection from '@/components/landing/PricingSection'
 import WhatToAsk from '@/components/landing/WhatToAsk'
 import React from 'react'
+
 
 const page = () => {
   return (
@@ -15,6 +17,7 @@ const page = () => {
       <WhatToAsk/>
       <PricingSection/>
       <CTA/>
+      <Footer/>
     </div>
 
   )
