3968bc8690f84718e0877cd5a70ecd28e71d778b
anshaf-thanish <anshafthanish@gmail.com>
2026-08-08 12:54:31 +0530

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/landing/Header'
+import Hero from '@/components/landing/Hero'
 import React from 'react'
 
 const page = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header/>
+      <Hero/>
     </div>
 
   )
